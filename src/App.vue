81b64225ba0a0c44e407da92aa4ded0c69818e6d
takashi <pero857942632@gmail.com>
2023-11-15 21:18:43 +0900

    List3-18を実装

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <HelloWorld title="Hello" message="※これは、Vue3のサンプルプロジェクトです。" />
+    <HelloWorld title="Hello" />
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,11 @@
   <div class="alert alert-info">
     <h2>{{ title }}</h2>
     <p>{{ message }}</p>
+    <hr>
+    <div>
+      <input class="form-control" type="text" v-model="input">
+      <button class="btn btn-info mt-2" v-on:click="doAction">Click</button>
+    </div>
   </div>
 </template>
 
@@ -11,6 +16,17 @@ export default {
   props: {
     title: String,
     message: String
+  },
+  data() {
+    return {
+      message: 'お名前は？',
+      input: 'no name',
+    }
+  },
+  methods: {
+    doAction() {
+      this.message = 'こんにちは、' + this.input + 'さん！'
+    }
   }
 }
 </script>
