dd4ef43daf150e0488d4abe76597ed494fab441b
takashi <pero857942632@gmail.com>
2023-11-17 21:55:49 +0900

    List3-23を実装

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,38 @@
 <template>
   <div class="alert alert-info">
     <h2>{{ title }}</h2>
-    <p>{{ message }}</p>
-    <hr>
+    <p ref="msg">{{ message }}</p>
+    <button class="btn btn-primary" v-on:click="doAction">Click</button>
+    <!-- <hr>
     <div>
       <input class="form-control" type="text" v-model="input">
       <button class="btn btn-info mt-2" v-on:click="doAction">Click</button>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    title: String,
-  },
+  // props: {
+  //   title: String,
+  // },
   data() {
     return {
-      message: 'お名前は？',
-      input: 'no name',
+      title: 'HelloWorld',
+      message: 'This is sample message.',
+      // input: 'no name',
     }
+  },
+  mounted() {
+    this.counter = 0
   },
   methods: {
     doAction() {
-      this.message = 'こんにちは、' + this.input + 'さん！'
-      this.$emit('result-event', this.input)
+      this.counter++
+      this.$refs.msg.innerHTML += '<h6>counted: ' + this.counter + '</h6>'
+      // this.message = 'こんにちは、' + this.input + 'さん！'
+      // this.$emit('result-event', this.input)
     }
   }
 }
