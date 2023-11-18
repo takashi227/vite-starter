ffecba84a5964666d03536367ba632fa50dc6f40
takashi <pero857942632@gmail.com>
2023-11-18 22:42:20 +0900

    List3-24wo

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,56 @@
 <template>
   <div id="app">
-    <HelloWorld v-bind:title="message" v-on:result-event="appAction" />
-    <hr>
-    <p>{{ result }}</p>
+    <Calc v-bind:title="message" v-on:result-event="appAction" />
+    <div class="mt-3 text-left">
+      <table class="table" v-html="log"></table>
+    </div>
+    <div>
+      <button class="btn btn-danger" v-on:click="doClear">Clear Log</button>
+    </div>
   </div>
-  <!-- <button class="btn btn-primary" v-on:click="doAction">change title</button> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calc
   },
   data() {
     return {
-      message: 'HELLO',
-      result: 'no event',
+      message: 'CALC',
+      result: [],
     }
   },
+  computed: {
+    log() {
+      let table = '<tr><th>Expression</th><th>Value</th></tr>'
+      if (this.result.length > 0) {
+        for (var i in this.result) {
+          table += '<tr><td>' + this.result[i][0] + '</td><th>' + this.result[i][1] + '</th></tr>'
+        }
+      }
+      return table
+    }
+  },
+  created() {
+    let items = localStorage.getItem('log')
+    let logs = JSON.parse(items)
+    if (logs != null) { this.result = logs }
+  },
   methods: {
-    // doAction() {
-    //   var input = prompt("new title:")
-    //   this.message = input
-    // }
-    appAction(message) {
-      this.result = '(*** you send"' + message + '". ***)'
+    appAction(exp, res) {
+      this.result.unshift([exp, res])
+      var log = JSON.stringify(this.result)
+      localStorage.setItem('log', log)
+    },
+    doClear() {
+      if (confirm('ログを全て消去します。')) {
+        localStorage.removeItem('log')
+        this.result = []
+      }
     }
   }
 }
