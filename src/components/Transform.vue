aee8a11bbbe97386d300c57869ac9b37b7ca8ba5
takashi <pero857942632@gmail.com>
2023-11-29 23:09:36 +0900

    アニメーション

--- src/components/Transform.vue
+++ src/components/Transform.vue
@@ -29,14 +29,14 @@ export default {
 </script>
 
 <style>
-.transit-enter-active {
+/* .transit-enter-active {
     transition: 1.0s;
-}
-.transit-leave-active {
+} */
+/* .transit-leave-active {
     transition: 1.0s;
     opacity: 0.5;
-}
-.transit-enter, .transit-leave-to {
+} */
+/* .transit-enter, .transit-leave-to { */
     /* 平行移動
     transform: translateX(200px) translateY(-200px);
     opacity: 0.1; 
@@ -46,7 +46,42 @@ export default {
     opacity: 0.1;
     */
     /* 回転 */
-    transform: rotateZ(360deg);
+    /* transform: rotateZ(360deg);
     opacity: 0.1;
+} */
+
+/* キーフレームによるアニメーション */
+.transit-leave-active {
+    animation: anim 5.0s;
+}
+.transit-enter-active {
+    animation: anim 2.5s reverse;
+}
+@keyframes anim {
+    0% {
+        transform: translateX(0px) translateY(0px) rotateZ(0deg);
+        opacity: 1.0;
+        background-color: #ddf;
+    }
+    25% {
+        transform: translateX(250px) translateY(0px) rotateZ(0deg);
+        opacity: 1.0;
+        background-color: #fdd;
+    }
+    50% {
+        transform: translateX(0px) translateY(-100px) rotateZ(540deg);
+        opacity: 1.0;
+        background-color: #dfd;
+    }
+    75% {
+        transform: translateX(250px) translateY(-100px) rotateZ(540deg);
+        opacity: 1.0;
+        background-color: #fdf;
+    }
+    100% {
+        transform: translateX(0px) translateY(-200px) rotateZ(1080deg);
+        opacity: 0;
+        background-color: #ffd;
+    }
 }
 </style>
